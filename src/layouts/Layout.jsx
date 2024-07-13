6f36fc3bc66e74a6223fdaa8a39e44d4f0d4af76
kyawlinthu123 <kyawlinthu655321@gmail.com>
2024-07-13 07:29:07 +0700

    Transition Effect added

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
-import NavBar from '../components/NavBar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
+import Navbar from '../components/NavBar';
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
+import './styles.css'
 
 export default function Layout() {
-  return (
-    <div>
-        <NavBar />
-        <div className='container mx-auto'>
-            <Outlet />
+
+    const location = useLocation();
+
+    return (
+        <div>
+            <Navbar />
+            <SwitchTransition>
+                <CSSTransition timeout={200} classNames='fade' key={location.pathname}>
+                    <div className='container mx-auto'>
+                        <Outlet />
+                    </div>
+                </CSSTransition>
+            </SwitchTransition>
+
         </div>
-    </div>
-  )
+    )
 }
